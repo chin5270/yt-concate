cb88c142147e8e0af82a8acd49f3f86f712cb1e7
chin5270 <crazy23123456@gmail.com>
2023-05-02 16:48:12 +0800

    utils

--- yt_concate/utils.py
+++ yt_concate/utils.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('./')
 import os
+from yt_dlp import YoutubeDL
 
 from yt_concate.settings import DOWNLOADS_DIR
 from yt_concate.settings import VIDEOS_DIR
@@ -17,3 +18,12 @@ class Utils:
         os.makedirs(DOWNLOADS_DIR,exist_ok=True)
         os.makedirs(VIDEOS_DIR,exist_ok=True)
         os.makedirs(CAPTIONS_DIR,exist_ok=True)
+
+    @staticmethod
+    def get_video_id(url):
+        return url.split('=')[-1]
+    
+
+    def caption_file_exist(self,url):
+        path = os.path.join(CAPTIONS_DIR,self.get_video_id(url)+ '.en.srt')
+        return os.path.exists(path) and os.path.getsize(path) > 0
