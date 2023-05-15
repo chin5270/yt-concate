bca2a844dd2d916bbafc4e43461b5c09b1a7df91
chin5270 <crazy23123456@gmail.com>
2023-05-15 13:00:26 +0800

    search

---
+++ yt_concate/pipeline/steps/search.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('./')
+from pprint import pprint
+
+from yt_concate.pipeline.steps.step import Step
+from yt_concate.module.found import Found
+
+class Search(Step):
+    def process(self, data, inputs, utils):
+        search_word = inputs['search_word']
+        found = []
+        for yt in data:
+            captions = yt.captions
+            if not captions:
+                continue
+            for caption in captions:
+                if search_word in caption:
+                    time = captions[caption]
+                    f = Found(yt,caption,time)
+                    found.append(f)
+        pprint(found)
+        return found
